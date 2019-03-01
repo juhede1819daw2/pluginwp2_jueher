eb31a644c96085dfaac59503671c32b7651456e6
root <root@inf1-juhede.fjeclot.net>
2019-03-01 19:15:29 +0100

    Commit 2

--- peupagina.php
+++ peupagina.php
@@ -8,4 +8,13 @@ Author: Julio Heredia Del Olmo
 Author URI: https://github.com/juhede1819daw2
 License: GNU GPLv3
 */
+
+function admin_menu_peu_pagina(){ 
+$titolPagina="Peu de pàgina per defecte";
+$titolEntrada="Peu de pàgina per defecte";
+$role="administrator";
+$id="peu_pagina_per_defecte";
+add_menu_page($titolPagina,$titolEntrada,$role,$id); ;
+}
+add_action('admin_menu','admin_menu_peu_pagina');
 ?>
