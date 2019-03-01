c5c5b14feabb30141eec59c221d45facc01ce6f6
root <root@inf1-juhede.fjeclot.net>
2019-03-01 19:01:17 +0100

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades
+Plugin URI: https://github.com/juhede1819daw2/pluginwp2_jueher
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Julio Heredia Del Olmo
+Author URI: https://github.com/juhede1819daw2
+License: GNU GPLv3
+*/
+?>
