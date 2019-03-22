3f2cb8020457e5842f2efd797d416d5d042747f9
root <root@inf1-juhede.fjeclot.net>
2019-03-22 15:33:44 +0100

    commit 3

--- peupagina.php
+++ peupagina.php
@@ -1,15 +1,21 @@
 <?php
 /*
-Plugin Name: Peu de pàgina per defecte per a noves entrades
-Plugin URI: https://github.com/juhede1819daw2/pluginwp2_jueher
+Plugin Name: Peu de pàgina per defecte per noves entrades
+Plugin URI: 
+Escriu l'adreça URL completa del teu plugin dins de Github
 Description: Crea peus de pàgina per defecte per a noves entrades
 Version: 0.1
-Author: Julio Heredia Del Olmo
-Author URI: https://github.com/juhede1819daw2
+Author: 
+El teu nom i cognom
+Author URI: 
+L'adreça URL del teu compte de Github
 License: GNU GPLv3
 */
+function nouPeuPagDef(){
+include_once('formPeuPag.php');
+}
 
-function admin_menu_peu_pagina(){
+function admin_menu_peu_pagina(){ 
 $titolPagina="Peu de pàgina per defecte";
 $titolEntrada="Peu de pàgina per defecte";
 $role="administrator";
@@ -17,10 +23,12 @@ $id="peu_pagina_per_defecte";
 $nom_funcio="nouPeuPagDef";
 add_menu_page($titolPagina,$titolEntrada,$role,$id, $nom_funcio);
 }
-
-function nouPeuPagDef(){
-include_once('formPeuPag.php');
+add_action('admin_menu','admin_menu_peu_pagina');
+add_action('wp_footer','afegPeuPagDef');
+function afegPeuPagDef(){
+echo "<div style='color: red;
+font-size: 10px;
+margin: 10px;'>".get_option('footer_text')."</div>";
 }
 
-add_action('admin_menu','admin_menu_peu_pagina');
 ?>
